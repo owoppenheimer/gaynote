3d157b0e76439eb618f3cd1c5b1630357c69e833
owoppenheimer <owoppenheimer@gmail.com>
2025-07-24 16:26:45 +0500

    add this shit

---
+++ src/main.rs
@@ -0,0 +1,76 @@
+use std::error::Error;
+use std::io::{self, Write};
+use std::{process::exit, env};
+use {rand, colored::Colorize};
+
+// define a vcersion
+const VERSION: &'static str = "1.0";
+
+fn input() -> Result<String, Box<dyn Error>> {
+    let mut i = String::new();
+    io::stdin().read_line(&mut i)?;
+    Ok(i)
+}
+
+fn args() -> Vec<String> {
+    let args: Vec<String> = env::args().skip(1).collect();
+    if args.is_empty() {
+        eprintln!("ERROR: Программе не переданы аргументы! Как я могу узнать кто из вас гей? :(");
+        eprintln!("Пример: {} {} {} ..", "gaynote".yellow(), "<Имя>".green(), "<Второе имя>".green());
+        eprintln!("\nЧтобы узнать больше, напишите --help.");
+        exit(1);
+    }
+
+    // find flags
+    for i in 0..args.len() {
+        match args.get(i).map(|s| s.as_str()) {
+            Some("--help") | Some("-h") => {
+                println!("{}", "Справка:".yellow());
+                println!("{}", "-----------".yellow());
+                println!("\n{} - {}я, ОтечестВеннАя прОгрАммА, которая позволяет вычислить гея, который отправится на СВО следующим!", "gaynote".green(), "СВО".red());
+                println!("Просто напишите имена пидорков через пробел.");
+                println!("Чтобы узнать версию, используйте флаг '--version' или '-ZV'");
+                exit(0);
+            },
+            Some("--version") | Some("-ZV") => {
+                println!("Версия {}ей прОгрАммы - {}", "СВО".red(), VERSION.yellow());
+                exit(0);
+            }
+            _ => {}
+        };
+    }
+    args
+}
+fn main() -> Result<(), Box<dyn Error>> {
+    let gays = args();
+
+    let r = rand::random_range(0..gays.len());
+    println!("\n{}", "- Геи:".red());
+    for i in 0..gays.len() {
+        println!("| {}: {}", gays[i].yellow(), i + 1);
+    }
+
+    print!("Вы готовы? [Y/n]: ");
+    io::stdout().flush()?;
+
+    let sex = input()?.trim().parse::<char>();
+    let sex = match sex {
+        Ok(sex) => sex,
+        Err(_) => 'y'
+    };
+
+    if let 'y' = sex.to_ascii_lowercase() {
+        for i in 0..gays.len() {
+            if r == i {
+                println!("{}", "-------------------------".yellow());
+                println!("\nГей: {}{}", gays[i].green(), "✅".green());
+                exit(0);
+            }
+        }
+    } else {
+        eprintln!("лох ебаный");
+        exit(1);
+    }
+
+    Ok(())
+}
